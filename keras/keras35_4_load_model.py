7bb9fc5f2c069e88b3e42874e2c73234d0d227b2
SunghoonSeok <76455292+SunghoonSeok@users.noreply.github.com>
2021-01-07 17:34:55 +0900

    01/07

---
+++ keras/keras35_4_load_model.py
@@ -0,0 +1,49 @@
+import numpy as np
+a = np.array(range(1,11))
+size = 5
+
+def split_x(seq, size):
+    aaa = []
+    for i in range(len(seq)-size+1):
+        subset = seq[i : (i+size)]
+        aaa.append([item for item in subset])
+    print(type(aaa))
+    return np.array(aaa)
+
+dataset = split_x(a, size)
+print("==================")
+print(dataset)
+x = dataset[:,:4]
+y = dataset[:,4] 
+x = x.reshape(x.shape[0],x.shape[1],1)
+
+# 2. 모델 구성
+from tensorflow.keras.models import load_model
+
+model = load_model('./model/save_keras35.h5')
+# 모델 추가되는지 테스트
+from tensorflow.keras.layers import Dense
+model.add(Dense(5, name='add_dense1'))  # model load해서 가져올때, 이름 겹치는거 주의
+model.add(Dense(1, name='add_dense2'))
+##########################
+
+
+model.summary()
+
+# 3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+from tensorflow.keras.callbacks import EarlyStopping
+early_stopping = EarlyStopping(monitor='loss', patience=20, mode='auto')
+model.fit(x, y, epochs=2000, batch_size=8, callbacks=[early_stopping])
+
+
+
+# 4. 평가, 예측
+loss = model.evaluate(x, y, batch_size=8)
+
+
+x_pred = dataset[-1,1:]
+x_pred = x_pred.reshape(1,4,1)
+y_pred = model.predict(x_pred)
+print(y_pred)
+
---
+++ keras/keras36_hist0.py
@@ -0,0 +1,56 @@
+import numpy as np
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, LSTM
+
+a = np.array(range(1,101))
+size = 5
+
+def split_x(seq, size):
+    aaa = []
+    for i in range(len(seq)-size+1):
+        subset = seq[i : (i+size)]
+        aaa.append([item for item in subset])  # [subset]과의 차이는?
+    print(type(aaa))
+    return np.array(aaa)
+
+dataset = split_x(a, size)
+
+x = dataset[:, :-1]
+y = dataset[:, -1]
+
+x = x.reshape(x.shape[0], x.shape[1], 1)
+
+pred = split_x(range(96,106),5)
+x_pred = pred[:, :-1]
+
+# 2. 모델 구성
+model = load_model('./model/save_keras35.h5')
+model.add(Dense(5,name='add_model_1'))
+model.add(Dense(1,name='add_model_2'))
+
+from tensorflow.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='loss', patience=20, mode='auto')
+
+# 3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam', metrics=['acc'])
+hist = model.fit(x, y, epochs=100, batch_size=32, verbose=1, validation_split=0.2, callbacks=[es])
+print(hist)
+print(hist.history.keys()) # loss, acc, val_loss, val_acc
+
+print(hist.history['loss'])
+
+# 그래프
+import matplotlib.pyplot as plt
+
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.plot(hist.history['acc'])
+plt.plot(hist.history['val_acc'])
+
+plt.title('loss & acc')
+plt.ylabel('loss, acc')
+plt.xlabel('epochs')
+plt.legend(['train loss', 'val loss', 'train acc', 'val acc'])
+plt.show()
+
+
