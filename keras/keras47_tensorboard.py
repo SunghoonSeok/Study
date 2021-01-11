62baccc46d3079c25fe182908324c26a0c0bebf0
SunghoonSeok <76455292+SunghoonSeok@users.noreply.github.com>
2021-01-11 18:08:59 +0900

    01/11

---
+++ keras/keras47_tensorboard.py
@@ -0,0 +1,100 @@
+# 인공지능계의 hello world라 불리는 mnist!!!
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.datasets import mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+print(x_train.shape, y_train.shape) #(60000, 28, 28) (60000,)
+print(x_test.shape, y_test.shape)   #(10000, 28, 28) (10000,)
+
+print(x_train[0])
+print(y_train[0])
+print(x_train[0].shape) # (28, 28)
+
+# plt.imshow(x_train[0], 'gray')
+# # plt.imshow(x_train[0])
+# plt.show()
+
+x_train = x_train.reshape(60000, 28, 28, 1).astype('float32')/255.  # 전처리
+x_test = x_test.reshape(10000, 28, 28, 1)/255.  # 전처리
+
+# OneHotEncoding
+from sklearn.preprocessing import OneHotEncoder
+
+y_train = y_train.reshape(-1,1) # reshape에서 -1은 재배열의 의미이다.
+y_test = y_test.reshape(-1,1)
+
+ohencoder = OneHotEncoder()
+ohencoder.fit(y_train)
+y_train = ohencoder.transform(y_train).toarray()
+y_test = ohencoder.transform(y_test).toarray()
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+
+model = Sequential()
+model.add(Conv2D(filters=100, kernel_size=(4, 4), padding='same',
+                 strides=1, input_shape=(28, 28, 1)))
+model.add(MaxPooling2D(pool_size=6))
+# model.add(Dropout(0.2))
+model.add(Conv2D(50, 2))
+model.add(Flatten())
+model.add(Dense(40, activation='relu'))
+model.add(Dense(30, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+# 3. 컴파일, 훈련
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint , TensorBoard
+early_stopping = EarlyStopping(monitor='val_loss', patience=5, mode='auto')
+modelpath= './modelCheckpoint/k45_mnist_{epoch:02d}-{val_loss:.4f}.hdf5'
+cp = ModelCheckpoint(modelpath, monitor='val_loss', save_best_only=True, mode='auto')
+tb = TensorBoard(log_dir='./graph', histogram_freq=0, write_graph=True, write_images=True)
+hist = model.fit(x_train, y_train, batch_size=128, epochs=70, validation_split=0.2, callbacks=[early_stopping, cp, tb])
+
+# 4. 평가, 예측
+result = model.evaluate(x_test, y_test, batch_size=128)
+y_pred = model.predict(x_test[:-10])
+
+print("loss : ", result[0])
+print("accuracy : ", result[1])
+
+# 시각화
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 6))
+plt.subplot(2, 1, 1)   # 2행 1열중 첫번째
+plt.plot(hist.history['loss'], marker='.', c='red', label='loss')
+plt.plot(hist.history['val_loss'], marker='.', c='blue', label='val_loss')
+plt.grid()
+
+plt.title('Cost Loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(loc='upper right')
+
+plt.subplot(2, 1, 2) #2행 1열중 두번째
+plt.plot(hist.history['accuracy'], marker='.', c='red', label='accuracy')
+plt.plot(hist.history['val_accuracy'], marker='.', c='blue', label='val_accuracy')
+plt.grid()
+
+plt.title('Acurracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(loc='upper right')
+
+plt.show()
+
+# poolsize=7, conv2d 두번째 50, 첫 dense=40
+# loss :  0.06115453317761421
+# acc :  0.9900000095367432
+
+# poolsize=6
+# loss :  0.05597861856222153
+# acc :  0.9905999898910522
