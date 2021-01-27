579ff3e0310e7115acd0e0bd58a875c6a712e871
SunghoonSeok <76455292+SunghoonSeok@users.noreply.github.com>
2021-01-27 15:35:14 +0900

    01/27

---
+++ ml/m05_cancer.py
@@ -0,0 +1,54 @@
+import numpy as np
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, 
+
+from sklearn.svm import LinearSVC, SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# 1. 데이터
+dataset = load_breast_cancer()
+x = dataset.data
+y = dataset.target
+
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, shuffle=True)
+
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+# 2. 모델 구성
+
+model = SVC()
+
+
+# 3. 컴파일, 훈련
+model.fit(x_train, y_train)
+# 4. 평가, 예측
+y_pred = model.predict(x_test)
+result = model.score(x_test, y_test)
+print(result)
+acc = accuracy_score(y_test, y_pred)
+print(acc)
+
+# MinMax
+# Linear SVC 0.9649122807017544
+# SVC 0.9824561403508771
+# KNeighborsClassifier 0.9736842105263158
+# RandomForestClassifier 0.9649122807017544
+# DecisionTreeClassifier 0.956140350877193 
+
+# Standard
+# Linear SVC 0.9736842105263158
+# SVC 0.9736842105263158
+# KNeighborsClassifier 0.9824561403508771
+# RandomForestClassifier 0.956140350877193
+# DecisionTreeClassifier 0.956140350877193
---
+++ ml/m06_wine.py
@@ -0,0 +1,57 @@
+import numpy as np
+from sklearn.datasets import load_wine
+from sklearn.preprocessing import StandardScaler, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+from sklearn.svm import LinearSVC, SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# 1. 데이터
+dataset = load_wine()
+x = dataset.data
+y = dataset.target
+
+print(x.shape, y.shape) #(150, 4),  (150, )
+print(x[:5])
+print(y)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, shuffle=True)
+
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+# 2. 모델 구성
+
+model = SVC()
+
+
+# 3. 컴파일, 훈련
+model.fit(x_train, y_train)
+# 4. 평가, 예측
+y_pred = model.predict(x_test)
+result = model.score(x_test, y_test)
+print(result)
+acc = accuracy_score(y_test, y_pred)
+print(acc)
+
+# MinMax
+# Linear SVC 1.0
+# SVC 1.0
+# KNeighborsClassifier 0.9722222222222222
+# RandomForestClassifier 1.0
+# DecisionTreeClassifier 0.9722222222222222
+
+# Standard
+# Linear SVC 1.0
+# SVC 0.97222222222222220
+# KNeighborsClassifier 0.9722222222222222
+# RandomForestClassifier 0.9722222222222222
+# DecisionTreeClassifier 0.9722222222222222
