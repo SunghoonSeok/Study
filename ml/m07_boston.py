716db3608a8cde1ff300facbd0c06c1838faa99d
SunghoonSeok <76455292+SunghoonSeok@users.noreply.github.com>
2021-01-27 16:32:59 +0900

    01/27

--- ml/m07_boston.py
+++ ml/m07_boston.py
@@ -4,10 +4,11 @@ from sklearn.preprocessing import StandardScaler, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, r2_score
 
-from sklearn.svm import LinearSVC, SVC, LinearSVR, SVR
+# from sklearn.svm import LinearSVC, SVC, LinearSVR, SVR
 from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
 
 # 1. 데이터
 dataset = load_boston()
@@ -18,35 +19,55 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, shuffle=True)
 
 
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
 scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 
 # 2. 모델 구성
-
-model = DecisionTreeRegressor()
-
-# 3. 컴파일, 훈련
-model.fit(x_train, y_train)
-# 4. 평가, 예측
-y_pred = model.predict(x_test)
-result = model.score(x_test, y_test)
-print(result)
-acc = r2_score(y_test, y_pred)
-print(acc)
+models =[LinearRegression(), RandomForestRegressor(), DecisionTreeRegressor(), KNeighborsRegressor()]
+for i in models:
+    model = i
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    result = model.score(x_test, y_test)
+    print(f'\n{i}')
+    print("model.score :",result)
+    acc = r2_score(y_test, y_pred)
+    print("r2_score :",acc)
 
 # MinMax
-# Linear SVR 0.3072748836901975  0.3072748836901975
-# SVR 0.1401655821255935  0.1401655821255935
-# KNeighborsRegressor  0.40380234136299853  0.40380234136299853
-# RandomForestRegressor 0.44254750464367854  0.4425475046436785
-# DecisionTreeRegressor -0.08404261371933663  -0.08404261371933663
+# LinearRegression()
+# model.score : 0.6648002998142213
+# r2_score : 0.6648002998142213
+
+# RandomForestRegressor()
+# model.score : 0.8859948974700671
+# r2_score : 0.8859948974700671
+
+# DecisionTreeRegressor()
+# model.score : 0.7834901613329981
+# r2_score : 0.7834901613329981
+
+# KNeighborsRegressor()
+# model.score : 0.7326477193079846
+# r2_score : 0.7326477193079846
+
 
 # Standard
-# Linear SVR 0.4517761589389889  0.4517761589389889
-# SVR 0.16028890079722924  0.16028890079722924
-# KNeighborsRegressor 0.3255734870306528  0.3255734870306528
-# RandomForestRegressor 0.42487762961558095  0.42487762961558095
-# DecisionTreeRegressor -0.35932846024789167  -0.35932846024789167
+# LinearRegression()
+# model.score : 0.7796055991479827
+# r2_score : 0.7796055991479827
+
+# RandomForestRegressor()
+# model.score : 0.876522103679384
+# r2_score : 0.876522103679384
+
+# DecisionTreeRegressor()
+# model.score : 0.6541205205234828
+# r2_score : 0.6541205205234828
+
+# KNeighborsRegressor()
+# model.score : 0.8085705151414402
+# r2_score : 0.8085705151414402
--- ml/m08_diabetes.py
+++ ml/m08_diabetes.py
@@ -8,7 +8,7 @@ from sklearn.svm import LinearSVC, SVC, LinearSVR, SVR
 from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-
+from sklearn.linear_model import LinearRegression
 # 1. 데이터
 dataset = load_diabetes()
 x = dataset.data
@@ -18,35 +18,54 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, shuffle=True)
 
 
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
 scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 
 # 2. 모델 구성
-
-model = DecisionTreeRegressor()
-
-# 3. 컴파일, 훈련
-model.fit(x_train, y_train)
-# 4. 평가, 예측
-y_pred = model.predict(x_test)
-result = model.score(x_test, y_test)
-print(result)
-acc = r2_score(y_test, y_pred)
-print(acc)
+models =[LinearRegression(), RandomForestRegressor(), DecisionTreeRegressor(), KNeighborsRegressor()]
+for i in models:
+    model = i
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    result = model.score(x_test, y_test)
+    print(f'\n{i}')
+    print("model.score :",result)
+    acc = r2_score(y_test, y_pred)
+    print("r2_score :",acc)
 
 # MinMax
-# Linear SVR 0.3072748836901975  0.3072748836901975
-# SVR 0.1401655821255935  0.1401655821255935
-# KNeighborsRegressor  0.40380234136299853  0.40380234136299853
-# RandomForestRegressor 0.44254750464367854  0.4425475046436785
-# DecisionTreeRegressor -0.08404261371933663  -0.08404261371933663
+# LinearRegression()
+# model.score : 0.40729129662251917
+# r2_score : 0.40729129662251917
+
+# RandomForestRegressor()
+# model.score : 0.42481851199779685
+# r2_score : 0.42481851199779685
+
+# DecisionTreeRegressor()
+# model.score : -0.11309841650939156
+# r2_score : -0.11309841650939156
+
+# KNeighborsRegressor()
+# model.score : 0.33049065564097546
+# r2_score : 0.33049065564097546
 
 # Standard
-# Linear SVR 0.4517761589389889  0.4517761589389889
-# SVR 0.16028890079722924  0.16028890079722924
-# KNeighborsRegressor 0.3255734870306528  0.3255734870306528
-# RandomForestRegressor 0.42487762961558095  0.42487762961558095
-# DecisionTreeRegressor -0.35932846024789167  -0.35932846024789167
+# LinearRegression()
+# model.score : 0.5697064035593562
+# r2_score : 0.5697064035593562
+
+# RandomForestRegressor()
+# model.score : 0.5310880675967731
+# r2_score : 0.5310880675967731
+
+# DecisionTreeRegressor()
+# model.score : -0.05170994009999097
+# r2_score : -0.05170994009999097
+
+# KNeighborsRegressor()
+# model.score : 0.48762179487052615
+# r2_score : 0.48762179487052615
