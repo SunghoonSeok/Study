8c2074820548df28367a6be505e36ae4ad353f59
SunghoonSeok <76455292+SunghoonSeok@users.noreply.github.com>
2021-02-17 18:13:18 +0900

    02/17

--- test/test07_per_project7_classification2.py
+++ test/test07_per_project7_classification2.py
@@ -65,44 +65,49 @@ from tensorflow.keras.layers import Dense, Input, LSTM, Dropout, Conv1D, Flatten
 from tensorflow.keras.optimizers import Adam, Adagrad, Adamax, SGD, RMSprop, Adadelta, Ftrl, Nadam
 
 # model = Sequential()
-# model.add(Dense(512, input_shape=(x_train.shape[1],)))
-# model.add(LeakyReLU())
+# model.add(Dense(512, activation='relu', input_shape=(x_train.shape[1],)))
 # model.add(Dropout(0.2))
-# model.add(Dense(256))
-# model.add(LeakyReLU())
+# model.add(Dense(256, activation='relu'))
 # model.add(Dropout(0.2))
-# model.add(Dense(128))
-# model.add(LeakyReLU())
+# model.add(Dense(128, activation='relu'))
 # model.add(Dropout(0.2))
-# model.add(Dense(64))
-# model.add(LeakyReLU())
+# model.add(Dense(64, activation='relu'))
 # model.add(Dropout(0.2))
 # model.add(Dense(11, activation='softmax'))
 
 # optimizer = Adam(lr=0.002)
 # from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
-# es = EarlyStopping(monitor='val_loss',mode='min', patience=30)
-# rl = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10, mode='min')
-# modelpath = 'c:/data/music/checkpoint/checkpoint_{val_loss:.4f}-{val_accuracy:.4f}'
+# es = EarlyStopping(monitor='val_loss',mode='min', patience=40)
+# rl = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=12, mode='min')
+# modelpath = 'c:/data/music/checkpoint/checkpoint_{val_loss:.4f}-{val_accuracy:.4f}.hdf5'
 # mc = ModelCheckpoint(modelpath, monitor='val_loss',save_best_only=True, mode='min',verbose=1)
 
 
 # model.compile(loss='sparse_categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 # model.fit(x_train, y_train, batch_size=128, epochs=1000, validation_data=(x_val, y_val), callbacks=[es,rl,mc])
 
-model2 = load_model('c:/data/music/checkpoint/checkpoint_0.2972-0.9224.hdf5')
-test_loss, test_acc  = model2.evaluate(x_test, y_test, batch_size=128)
-print("The test Loss is :",test_loss)
-print("\nThe Best test Accuracy is :",test_acc*100)
-y_pred = model2.predict(x_pred)
-y_recovery = np.argmax(y_pred, axis=1).reshape(-1,1)
-print(y_recovery)
-y_recovery = index_label[y_recovery[0][0]]
+from xgboost import XGBClassifier, XGBRegressor
+model = XGBClassifier(n_estimators=1000, learning_rate=0.04,
+                    tree_method='gpu_hist',predictor='gpu_predictor', reg_lambda=3, reg_alpha=2, max_depth=9
+)
 
+model.fit(x_train,y_train, verbose=1,eval_metric='mlogloss',
+         eval_set=[(x_train,y_train),(x_val,y_val)],early_stopping_rounds=10
+)
 
+aaa = model.score(x_test,y_test)
+# test_loss, test_acc  = model.evaluate(x_test, y_test, batch_size=128)
+# print("The test Loss is :",test_loss)
+# print("\nThe Best test Accuracy is :",test_acc*100)
+y_pred = model.predict(x_pred)
+print(y_pred)
+
+# y_recovery = np.argmax(y_pred, axis=1).reshape(-1,1)
+# print(y_recovery)
+y_recovery = index_label[y_pred[0]]
 
 print(""+str(a[1])+" 는(은) 무슨 장르니?")
-print(""+str(a[1])+"는(은)",y_recovery,"장르입니다.")
+print(""+str(a[1])+" 는(은)",y_recovery,"장르입니다.")
 
 
 df_30 = pd.read_csv('c:/data/music/30s_data.csv')
@@ -126,4 +131,4 @@ def find_similar_songs(name, n=5):
     print("\n*******\n"+name+" 와(과) 비슷한 곡 추천해줘")
     print(""+name+" 와(과) 비슷한 곡 "+str(n)+"개의 list입니다.")
     print(series.head(n).to_frame('추천목록'))
-find_similar_songs(str(a[1]))
+find_similar_songs(a[1])
