d9a64a8a347d2b34363ed29dec11f28ee2931f83
SunghoonSeok <76455292+SunghoonSeok@users.noreply.github.com>
2021-06-03 12:33:37 +0900

    0603

--- ggulzam/add_sound.py
+++ ggulzam/add_sound.py
@@ -1,6 +1,6 @@
 from moviepy.editor import *
-videoclip = VideoFileClip("c:/video/coin_sketch_compress.mp4")
-audioclip = AudioFileClip("c:/video/coin_sound.mp3")
+videoclip = VideoFileClip("c:/video/eight_acoustic_sketch_compress.mp4")
+audioclip = AudioFileClip("c:/video/eight_acoustic_sound.mp3")
 new_audioclip = CompositeAudioClip([audioclip])
 videoclip.audio = new_audioclip
-videoclip.write_videofile("c:/video/coin_with_sound.mp4")
+videoclip.write_videofile("c:/video/eight_acoustic_with_sound.mp4")
--- ggulzam/compress.py
+++ ggulzam/compress.py
@@ -1,4 +1,4 @@
 import moviepy.editor as mp
-clip = mp.VideoFileClip("C:/video/coin_sketch.avi")
+clip = mp.VideoFileClip("C:/video/eight_acoustic_sketch.avi")
 clip_resized = clip.resize(height=720) # make the height 360px ( According to moviePy documenation The width is then computed so that the width/height ratio is conserved.)
-clip_resized.write_videofile("C:/video/coin_sketch_compress.mp4")
+clip_resized.write_videofile("C:/video/eight_acoustic_sketch_compress.mp4")
