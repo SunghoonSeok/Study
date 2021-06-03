d077f470ad4c16f215d5e03adae9ebf6f77eea4c
SunghoonSeok <76455292+SunghoonSeok@users.noreply.github.com>
2021-06-03 10:40:40 +0900

    0603

--- ggulzam/compress.py
+++ ggulzam/compress.py
@@ -1,4 +1,4 @@
-import moviepy.editor as mps
-clip = mp.VideoFileClip("C:/video/celebrity_sketch.avi")
+import moviepy.editor as mp
+clip = mp.VideoFileClip("C:/video/coin_sketch.avi")
 clip_resized = clip.resize(height=720) # make the height 360px ( According to moviePy documenation The width is then computed so that the width/height ratio is conserved.)
-clip_resized.write_videofile("C:/video/celebrity_sketch_compress.mp4")
+clip_resized.write_videofile("C:/video/coin_sketch_compress.mp4")
